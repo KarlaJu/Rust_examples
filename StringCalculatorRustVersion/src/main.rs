f163637fc6294157ec9c7465c238d516b6cf4bc3
karlaJu <karla@makingdevs.com>
2018-06-29 15:23:49 -0500

    Función para quitar los saltos de linea

--- StringCalculatorRustVersion/src/main.rs
+++ StringCalculatorRustVersion/src/main.rs
@@ -14,16 +14,22 @@ pub fn verify_the_string_is_empty(string: &str) -> bool{
 	return false;	
 }
 
+pub fn replace_break_lines_in_string(string: &str) -> String {
+    return string.replace("\n", ",");
+}
+
 pub fn do_the_string_calculator(string: &str) -> i32 {
-	let replace = string.replace("\n",",");
-	let split = replace.split(',');
+    let string_without_break_lines = replace_break_lines_in_string(string);
+	let split = string_without_break_lines.split(',');
 	let mut sum = 0;
 	for number_in_string in split {
 		let number = number_in_string.parse::<i32>().unwrap();
 		sum = sum + number;
 	}
-	return sum
+	return sum;
 }
+
+
 
 
 
@@ -40,12 +46,12 @@ mod tests {
 
     #[test]
     fn test_one_number() {
-    	let string = "7".to_string();
+    	let string = "1".to_string();
+        assert_eq!(1, add(&string));
+        let string = "3".to_string();
+        assert_eq!(3, add(&string));
+        let string = "7".to_string();
         assert_eq!(7, add(&string));
-        let string = "4".to_string();
-        assert_eq!(4, add(&string));
-        let string = "9".to_string();
-        assert_eq!(9, add(&string));
     }
 
     #[test]
@@ -62,8 +68,8 @@ mod tests {
     fn test_sum_of_any_numbers() {
     	let string = "1,2,3".to_string();
         assert_eq!(6, add(&string));
-        let string = "3,4,5,6,7,8,9".to_string();
-        assert_eq!(42, add(&string));
+        let string = "3,4,5,6,7,8".to_string();
+        assert_eq!(33, add(&string));
         let string = "1,1,1,1,1,1,1,1,1,1,1,1,1,1".to_string();
         assert_eq!(14, add(&string));
     }
@@ -72,9 +78,19 @@ mod tests {
     fn sum_any_numbers_with_break_lines() {
     	let string = "1,2\n3".to_string();
         assert_eq!(6, add(&string));
-        let string = "3,4\n5,6,7\n8,9".to_string();
+        let string = "3\n4,5\n6,7\n8".to_string();
+        assert_eq!(33, add(&string));
+        let string = "1\n1\n1\n1\n1,1,1,1,1,1\n1,1\n1,1".to_string();
+        assert_eq!(14, add(&string));
+    }
+
+   #[test]
+    fn sum_any_numbers_with_other_delimiters() {
+        let string = "//;\n1;2;3".to_string();
+        assert_eq!(6, add(&string));
+        let string = "//#\n3#4#5#6#7#8".to_string();
         assert_eq!(42, add(&string));
-        let string = "1\n1\n1\n1\n1,1,1,1,1,1\n1,1,1,1".to_string();
+        let string = "//|\n1|1|1|1|1|1|1|1|1|1|1|1|1|1".to_string();
         assert_eq!(14, add(&string));
     }
 }

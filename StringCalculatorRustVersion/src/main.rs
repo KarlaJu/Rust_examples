db948cf9fe2c5a2372a009fc0954a24816115897
karlaJu <karla@makingdevs.com>
2018-06-29 11:30:32 -0500

    Agregando pruebas

--- StringCalculatorRustVersion/src/main.rs
+++ StringCalculatorRustVersion/src/main.rs
@@ -15,15 +15,16 @@ pub fn verify_the_string_is_empty(string: &str) -> bool{
 }
 
 pub fn do_the_string_calculator(string: &str) -> i32 {
-  //for line in string.split(','){
-    //println!("{}", line);
-  //}
-  //let mut numbers = string.parse::<i32>().unwrap();
-  //  println!("{}",numbers );
-  //  return numbers;
-  //let vec =split.colletc::<Vec<&str>>();
-  let v = string.split(',').collect::<Vec<parse::<i32>().unwrap()>>;
+  let replace = string.replace("\n",",");
+  let split = replace.split(',');
+  let mut sum = 0;
+  for number_in_string in split {
+    let number = number_in_string.parse::<i32>().unwrap();
+    sum = sum + number;
+  }
+  return sum
 
+  }
 }
 
 
@@ -57,5 +58,25 @@ mod tests {
         assert_eq!(7, add(&string));
         let string = "7,8".to_string();
         assert_eq!(15, add(&string));
+    }
+
+    #[test]
+    fn any_numbers() {
+      let string = "1,2,3".to_string();
+        assert_eq!(6, add(&string));
+        let string = "3,4,5,6,7,8,9".to_string();
+        assert_eq!(42, add(&string));
+        let string = "1,1,1,1,1,1,1,1,1,1,1,1,1,1".to_string();
+        assert_eq!(14, add(&string));
+    }
+    
+    #[test]
+    fn any_numbers_with_break_lines() {
+      let string = "1,2\n3".to_string();
+        assert_eq!(6, add(&string));
+        let string = "3,4\n5,6,7\n8,9".to_string();
+        assert_eq!(42, add(&string));
+        let string = "1\n1\n1\n1\n1,1,1,1,1,1\n1,1,1,1".to_string();
+        assert_eq!(14, add(&string));
     }
 }

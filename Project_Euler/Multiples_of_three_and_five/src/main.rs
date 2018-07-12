926acc8b743fe9b732354a330e6d941fdaa324aa
karlaJu <karla@makingdevs.com>
2018-07-12 10:30:06 -0500

    Agregando ejemplo de project euler, números del 1 al 1,000 multiplos de 3 y 5

---
+++ Project_Euler/Multiples_of_three_and_five/src/main.rs
@@ -0,0 +1,19 @@
+pub fn add(int z:&int ) {
+    //Natural numbers, multiples of 3 or 5
+    let number_three;
+    let number_five;
+    if 
+    println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::add;
+
+    #[test]
+    fn using_empty_string() {
+    	let string = 3;
+      assert_eq!(0, add(&3));
+    }
+}
